6e06e8794b9af25bde34c3e60c474ebb46c1855c
Bogdan Bystritski <q80061@gmail.com>
2026-03-04 13:48:56 +0100

    Initial Commit

---
+++ DSContainerManager/DSContainerManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSContainerManagerApp.swift
+//  DSContainerManager
+//
+//  Created by Admin on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DSContainerManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
